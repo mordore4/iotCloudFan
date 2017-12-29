6f01db665de0fb1448c8d8441fffac6bafd1dc24
quasar_singularity <samdemeulenaere@hotmail.com>
2017-12-29 23:49:59 +0100

    fixed

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,7 +6,20 @@ router.post('/temperature', (req, res) => {
     let data = req.body;
     let session = req.session.userInfo;
     if (session != undefined) {
-        request(`https://api.thinger.io/v2/users/singulasar/devices/esp8266/wanted_temp`)
+        let options = {
+            url: `https://api.thinger.io/v2/users/${session.username}/devices/${session.device_code}/${session.resource}`,
+            headers: {
+                'Authorization': `Bearer ${session.auth}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"in": parseInt(data.temperature)})
+        }
+        console.log(options);
+        request.post(options, (error, response, body) => {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+        })
     } else {
         req.redirect("/login");
     }
