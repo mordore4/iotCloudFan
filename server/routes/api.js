f3a6ccbb114d7dc47c7b900fba92fb9f9434571c
quasar_singularity <samdemeulenaere@hotmail.com>
2017-12-29 23:58:55 +0100

    final

--- server/routes/api.js
+++ server/routes/api.js
@@ -14,14 +14,12 @@ router.post('/temperature', (req, res) => {
             },
             body: JSON.stringify({"in": parseInt(data.temperature)})
         }
-        console.log(options);
         request.post(options, (error, response, body) => {
             console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
         })
+        res.redirect("/settings");
     } else {
-        req.redirect("/login");
+        res.redirect("/login");
     }
 });
 
